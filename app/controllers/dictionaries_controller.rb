d1e85c44a214351be5ab315318566e430758d209
Joss Reeves <joseph.reeves@gmail.com>
2017-03-12 01:12:41 -0500

    enabled auth. minor cleanup. added rack middleware for CORS and configured it.

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -1,8 +1,6 @@
 class DictionariesController < ApplicationController
   include ActionController::HttpAuthentication::Token::ControllerMethods
-  #before_action :authenticate
-
-  TOKEN = "secret"
+  before_action :authenticate
 
   # 50/50. Half the responses are a random dictionary word, Half get a 503
   def shuffle
@@ -12,12 +10,14 @@ class DictionariesController < ApplicationController
     if success < 1
       return_unavailable
     else
-      @word = random
-      render json: @word
+      d = random
+      render plain: d.word
     end
   end
 
   private
+    TOKEN = "secret"
+
     # perform token auth
     def authenticate
       authenticate_or_request_with_http_token do |token, options|
@@ -29,7 +29,7 @@ class DictionariesController < ApplicationController
     end
 
     def return_unavailable
-      render :nothing => true, :status => :service_unavailable
+      head :service_unavailable
     end
 
     # grab a random dictionary word
--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,13 @@ module FiftyFifty
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     #config.middleware.use  ActionController::HttpAuthentication::Token
+
+    # Rack CORS config. So the AJAX stuff will work.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
